086fa91355ca1d5ba3658e7a599de0d83287e8a8
NoviceF <lforumuserl@mail.ru>
2015-07-17 18:53:45 +0300

    [*] combobox

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,27 +10,27 @@ MainWindow::MainWindow(QWidget *parent) :
     fsModel_(new QFileSystemModel(this)),
     statGetter_(new StatGetter(ui_->tableView, this))
 {
-    SetPositionCenter();
-
     ui_->setupUi(this);
+
+    SetPositionCenter();
 
 //    QFileSystemModel* model = new QFileSystemModel(this);
 
 //    model->setFilter(QDir::Drives);
 //    model->setRootPath("");
-    fsModel_->setRootPath("/home/novice/proj/cpp/dirtest");
+//    const QString selectedPath("/home/novice/proj/cpp/dirtest");
+//    fsModel_->setRootPath(selectedPath);
+
+    connect(ui_->comboBox, SIGNAL(currentIndexChanged(int)), this,
+            SLOT(setTreeRootIndex(int)));
+
+    QString selectedPath("d:\\tmp");
+    fsModel_->setRootPath(selectedPath);
     fsModel_->setFilter(QDir::Dirs | QDir::NoDotAndDotDot);
     ui_->comboBox->setModel(fsModel_);
     ui_->comboBox->setSizeAdjustPolicy(QComboBox::AdjustToContents);
-    ui_->comboBox->setRootModelIndex(fsModel_->index("/home/novice/proj/cpp/dirtest"));
+    ui_->comboBox->setRootModelIndex(fsModel_->index(selectedPath));
     ui_->comboBox->setCurrentIndex(0);
-//    ui_->comboBox->activated("");
-
-//    dirModel_->setRootPath("c:\\");
-    ui_->treeView->setModel(fsModel_);
-//    connect(ui_->comboBox, &QComboBox::activated, this, &MainWindow::setTreeRootIndex);
-    connect(ui_->comboBox, SIGNAL(currentIndexChanged(int)), this,
-            SLOT(setTreeRootIndex(int)));
 
     ui_->progressBar->setMinimum(0);
     ui_->progressBar->setMaximum(100);
@@ -47,34 +47,13 @@ MainWindow::~MainWindow()
 
 void MainWindow::setTreeRootIndex(int index)
 {
+    //TODO: правильно расположить слэши
     const QString itemName = ui_->comboBox->currentText();
-    QString selectedDirName("/home/novice/proj/cpp/dirtest");
-    selectedDirName = selectedDirName + "/" + itemName;
-    QDir selectedDir(selectedDirName);
-//    QModelIndex modIndex = fsModel_->index(itemName, 2);
-//    QModelIndex modIndex = fsModel_->index(QDir::currentPath());
-    QModelIndex modIndex = fsModel_->index(selectedDir.absolutePath());
-    ui_->treeView->setRootIndex(modIndex);
-
-//    if(fsModel_->canFetchMore(index))
-//    // make sure the entries in the dir are loaded
-//    fsModel_->fetchMore(index);
-////    ui_->treeView->sel
-
-
-
-
-//    qDebug() << "index " << index << "=" << fsModel_->filePath(index);
-
-//    ui_->comboBox->
-
-//    const int cbIndex = ui_->comboBox->currentIndex();
-//    const QString itemName = ui_->comboBox->itemData(cbIndex).toString();
-//    const int itemIntName = ui_->comboBox->itemData(cbIndex).toInt();
-    qDebug() << "itemName = " << itemName ;
-//    ui_->treeView->setRootIndex(fsModel_->index(index));
-
-//    ui->comboBox->setCurrentIndex(0);
+    const QString selectedPath(fsModel_->rootPath() + "/" + itemName);
+    QDir selectedDir(selectedPath);
+    ui_->treeView->setModel(fsModel_);
+//    ui_->treeView->setRootIndex(fsModel_->index(selectedDir.absolutePath()));
+    ui_->treeView->setCurrentIndex(fsModel_->index(selectedDir.absolutePath()));
 }
 
 void MainWindow::SetPositionCenter()
